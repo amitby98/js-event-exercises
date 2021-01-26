e799d75032eb050bea1d95f94fbc06cfd31a011d
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-27 01:33:56 +0200

    solved question 1 chapter 5

--- Chapter5/Selectable list/script.js
+++ Chapter5/Selectable list/script.js
@@ -1,1 +1,13 @@
+ul.addEventListener("click", (e) => {
+  if (!e.ctrlKey && !e.metaKey) {
+    for (let li of ul.children) {
+      li.classList.remove("selected");
+    }
+  }
+  if (e.target.tagName === "LI") {
+    e.target.classList.add("selected");
+  }
+});
 
+//Cancel double click
+ul.onmousedown = () => false;
