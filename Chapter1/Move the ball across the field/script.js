9eb65a3a82fcd0f1d24fcd8afb4be55accf75c73
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-26 12:32:24 +0200

    solved question 3 chapter 1

--- Chapter1/Move the ball across the field/script.js
+++ Chapter1/Move the ball across the field/script.js
@@ -1,3 +1,26 @@
-document.getElementById("button").addEventListener("click", (e) => {
-  document.getElementById("button").hidden = true;
+let field = document.getElementById("field");
+let ball = document.getElementById("ball");
+ball.style.top = 100 + "px";
+ball.style.left = 100 + "px";
+field.addEventListener("click", (event) => {
+  let ballX = ball.clientWidth;
+  let ballY = ball.clientHeight;
+  let cX = event.clientX;
+  let cY = event.clientY;
+  //Center ball on cursor
+  console.log(event.clientY);
+  if (cX < 38) {
+    cX = 38;
+  }
+  if (cX > 198) {
+    cX = 198;
+  }
+  if (cY < 75) {
+    cY = 75;
+  }
+  if (cY > 183) {
+    cY = 183;
+  }
+  ball.style.top = cY - ballY / 2 + "px";
+  ball.style.left = cX - ballX / 2 + "px";
 });
